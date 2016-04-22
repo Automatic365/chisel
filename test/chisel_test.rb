b0a32a3936cd5750912608e22dc049ada5693e62
Jason Hanna <jason.hanna01@gmail.com>
2016-04-22 08:47:10 -0600

    fixed formatting

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -3,9 +3,11 @@ require 'minitest/pride'
 require './lib/chisel'
 
 class ChiselTest < Minitest::Test
+
   def test_chisel_converts_markdown_to_html
-    markdown = "#i am a header\n\ni am a paragraph"
-    html = "<h1>i am a header</h1>\n\n<p>i am a paragraph</p>"
+    markdown = "#New York Knicks\n\nvillanova wildcats"
+    html = "<h1>New York Knicks</h1>\n\n<p>villanova wildcats</p>"
     assert_equal html, Chisel.new(markdown).to_html
   end
+
 end
